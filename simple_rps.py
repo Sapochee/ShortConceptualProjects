0ab5663583f5cffa7081587b270566860887e9ae
Sapochee <129986526+Sapochee@users.noreply.github.com>
2023-10-10 13:01:39 -0400

    Add files via upload

---
+++ simple_rps.py
@@ -0,0 +1,37 @@
+"""Game of rock paper scissors
+"""
+import random
+
+def get_choices():
+    options = ["rock", "paper", "scissors"]
+    player_choice = input("Enter a choice (rock, paper, scissors): ")
+    computer_choice = random.choice(options)
+    choices = {"player": player_choice, "computer": computer_choice}
+    return choices
+
+def check_winner(player, computer):
+    print(f"You chose {player}, computer chose {computer}")
+    if player == computer:
+        return "It's a tie!"
+    
+    elif player == "rock":
+        if computer == "scissors":
+            return "Rock smashes scissors! You win!"
+        else:
+            return "Paper covers rock! You lose."
+        
+    elif player == "paper":
+        if computer == "rock":
+            return "Paper covers rock! You win!"
+        else:
+            return "Scissors cuts paper! You lose."
+        
+    elif player == "scissors":
+        if computer == "paper":
+            return "Scissors cut paper! You win!"
+        else:
+            return "Rock smashes scissors! You lose."  
+        
+choices = get_choices()
+result = check_winner(choices["player"], choices["computer"])
+print(result)
